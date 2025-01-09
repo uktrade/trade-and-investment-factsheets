a84961edbd0a9697207bf46cd300e040939f3301
Michal Charemza <michal@charemza.name>
2025-01-09 14:20:36 +0000

    refactor: remove unused quota balances data loaders

--- src/data/quotas-including-current-volumes.csv.py
+++
@@ -1,9 +0,0 @@
-import sys
-import requests
-
-response = requests.get(
-    'https://data.api.trade.gov.uk/v1/datasets/uk-trade-quotas/versions/latest/reports/quotas-including-current-volumes/data',
-    params={'format': 'csv'},
-)
-response.raise_for_status()
-sys.stdout.buffer.write(response.content)
